686c95e60fa10ece976860bff51b6d97d9f22142
Rafael Batista <rafaelb24@yahoo.com>
2019-03-15 23:17:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-     @post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     @post.update(post_params(:title))
     redirect_to post_path(@post)
 	end
